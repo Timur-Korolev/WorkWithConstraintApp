09a46e0a23afd305a85eb7a95d7db931f23d31db
Timur Korolev <timurkorolev89@gmail.com>
2019-09-18 17:38:06 +0300

    Initial Commit

---
+++ WorkWithConstraintApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkWithConstraintApp
+//
+//  Created by admin on 9/18/19.
+//  Copyright © 2019 Timur Korolev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
